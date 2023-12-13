05053e3ab8919c36edf3a5e2663a6a289d30b421
Irene Chen <104665922+iskaikaila@users.noreply.github.com>
2023-12-13 15:10:07 -0800

    Update routing.module.ts

--- routing.module.ts
+++ routing.module.ts
@@ -6,11 +6,12 @@ import { PersonAddFormComponent } from './person-add-form/person-add-form.compon
 import { PersonViewComponent } from './person-view/person-view.component';
 // import { PersonEditComponent } from './person-edit/person-edit.component';
 
-const appRoutes:Routes = [
-  { path: 'people', component: PeopleListComponent },
-  { path: 'person/add', component: PersonAddFormComponent },
-  { path: 'person/:name', component: PersonViewComponent },
-  {path:'',redirectTo:'/people',pathMatch:'full'}
+// 定义应用程序的路由配置
+const appRoutes: Routes = [
+  { path: 'people', component: PeopleListComponent }, // 显示人员列表的路由
+  { path: 'person/add', component: PersonAddFormComponent }, // 显示添加人员表单的路由
+  { path: 'person/:name', component: PersonViewComponent }, // 显示个人资料的路由，其中":name"是动态参数
+  { path: '', redirectTo: '/people', pathMatch: 'full' } // 默认重定向到/people路由
 ]
 
 @NgModule({
