c9f9305bdd9cdabf3fcd4eee3dc80b66947bf830
Irene Chen <104665922+iskaikaila@users.noreply.github.com>
2023-12-13 15:24:28 -0800

    Update count-people.pipe.ts

--- count-people.pipe.ts
+++ count-people.pipe.ts
@@ -1,12 +1,14 @@
+// 导入Angular核心模块
 import { Pipe, PipeTransform } from '@angular/core';
 
+// 使用@Pipe装饰器定义一个新的管道
 @Pipe({
-  name: 'countPeople'
+  name: 'countPeople'  // 设置管道的名称为'countPeople'
 })
 export class CountPeoplePipe implements PipeTransform {
-
+  // 实现PipeTransform接口中的transform方法
   transform(people: any[]): number {
+    // 返回输入数组的长度
     return people.length;
   }
-
 }
