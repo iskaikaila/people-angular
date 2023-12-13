5eefc2e3dfdcfe0c5de05b541abbe966ba1c4d64
Irene Chen <104665922+iskaikaila@users.noreply.github.com>
2023-12-13 15:09:32 -0800

    Update search.pipe.ts

--- search.pipe.ts
+++ search.pipe.ts
@@ -5,10 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(people:any[],querystring:string): any {
+  // 这是一个搜索管道，用于过滤包含指定查询字符串的人员列表
+
+  transform(people: any[], querystring: string): any {
+    // 使用filter方法过滤人员列表，仅保留包含查询字符串的人员
     return people.filter(p => { 
-      return p.name.includes(querystring)
-    })
+      return p.name.includes(querystring);
+    });
   }
 
 }
